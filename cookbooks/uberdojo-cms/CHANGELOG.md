5427619e7268637d142cea189081f1e497a0185f
Tim Dettrick <tim@dettrick.net>
2012-09-25 11:19:22 +1000

    Basic reverse proxy setup.

---
+++ cookbooks/uberdojo-cms/CHANGELOG.md
@@ -0,0 +1,12 @@
+# CHANGELOG for uberdojo-cms
+
+This file is used to list changes made in each version of uberdojo-cms.
+
+## 0.1.0:
+
+* Initial release of uberdojo-cms
+
+- - - 
+Check the [Markdown Syntax Guide](http://daringfireball.net/projects/markdown/syntax) for help with Markdown.
+
+The [Github Flavored Markdown page](http://github.github.com/github-flavored-markdown/) describes the differences between markdown on github and standard markdown.
---
+++ cookbooks/uberdojo-cms/recipes/default.rb
@@ -0,0 +1,28 @@
+#
+# Cookbook Name:: uberdojo-cms
+# Recipe:: default
+#
+# Copyright 2012, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+include_recipe "yum::epel"
+
+package 'nginx'
+
+service 'iptables' do
+  supports :status => true, :restart => true, :reload => true
+  action [ :disable, :stop ]
+end
+
+service 'nginx' do
+  supports :status => true, :restart => true, :reload => true
+  action [ :enable, :start ]
+end
+
+template "/etc/nginx/conf.d/default.conf" do
+  mode "0644"
+  source "nginx_conf.erb"
+  notifies :restart, "service[nginx]"
+end
